3110daf804b62e7cf039640d3239f778999aea2f
UJ <uchonco21@gmail.com>
2025-02-04 10:04:56 +0200

    Updated

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function openLogin(){
+    
+    document.getElementById("openlogin").style.display = "flex"
+}
+
+function closeLogin(){
+    document.getElementById("openlogin").style.display = "none"
+}
